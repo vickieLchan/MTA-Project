c38613347df153103366700a2735d5d193c1526a
Yvonne Matos <29462745+yvonnekmatos@users.noreply.github.com>
2019-07-04 20:55:27 -0500

    clean and prepare income and lat long data per station

--- mta_data_cleaning.py
+++ mta_data_cleaning.py
@@ -3,7 +3,7 @@ import numpy as np
 import datetime as dt
 from datetime import datetime
 import seaborn as sns
-
+import censusgeocode as cg
 #=================================================================================================
 # GET RAW DATA
 def get_data(week_nums):
@@ -125,3 +125,96 @@ turn_shifted.exits_per_timestamp.max()
 turn_cleaned = turn_shifted.drop(columns=['prev_date', 'prev_entries', 'prev_exits'])
 turn_cleaned.to_csv(working_directory+'turnstiles_cleaned.csv', index=False)
 #=================================================================================================
+turn_cleaned = pd.read_csv(working_directory+'turnstiles_cleaned.csv', parse_dates=['date', 'date_time'])
+
+# Combine other useful datasets
+lat_long = pd.read_csv(working_directory+'NYC_Transit_Subway_Entrance_And_Exit_Data.csv')
+lat_long.columns = lat_long.columns.str.lower().str.strip()
+lat_long = lat_long.rename(columns={'station name': 'station_name'
+                                    , 'station latitude': 'station_latitude'
+                                    , 'station longitude': 'station_longitude'})
+lat_long.head(2)
+
+lat_long.shape
+lat_long = lat_long.iloc[:,:5]
+lat_long.head(2)
+lat_long.station_name.nunique()
+
+lat_long = lat_long.sort_values(by='station_name')
+lat_long = lat_long.drop_duplicates(subset='station_name', keep='first')
+print(len(lat_long))
+turn_cleaned.station.nunique()
+
+
+census = pd.read_csv(working_directory+'censustract-medianhouseholdincome2018.csv')
+census.columns = census.columns.str.lower().str.strip()
+census = census.rename(columns={'census tract': 'census_tract'})
+print(len(census))
+census[census.census_tract==36061018900]
+
+#=================================================================================================
+# Get census tract ID for each station lat and long
+census_dict = {}
+for idx, row in lat_long.iterrows():
+    census_info = cg.coordinates(y=row.station_latitude, x=row.station_longitude)
+    tract = census_info['Census Tracts'][0]['GEOID']
+    census_dict[tract] = [row.station_latitude, row.station_longitude]
+
+census_coord = pd.DataFrame(census_dict).T
+census_coord = census_coord.reset_index()
+census_coord = census_coord.rename(columns={'index': 'census_tract'
+                                            , 0: 'station_latitude'
+                                            , 1: 'station_longitude'})
+census_coord.shape
+census_coord.to_csv(working_directory+'census_tract_lat_long.csv', index=False)
+#=================================================================================================
+
+census_coord['census_tract'] = census_coord.census_tract.astype(int)
+census_coord.head()
+
+census.head(2)
+census = census.rename(columns={'2013-2017': 'med_income_2017'})
+census = census[['census_tract', 'med_income_2017']]
+census.head(2)
+
+income_lat_long = pd.merge(census, census_coord, on='census_tract', how='inner')
+income_lat_long.shape
+
+income_lat_long = income_lat_long.drop(columns='census_tract')
+income_lat_long.head(2)
+
+income_stations = pd.merge(lat_long, income_lat_long, on=['station_latitude', 'station_longitude'], how='left')
+income_stations.shape
+income_stations.head()
+
+income_stations = income_stations.drop(columns=['division', 'line'])
+income_stations.to_csv(working_directory+'stations_with_coord_and_income.csv', index=False)
+#=================================================================================================
+
+income_stations.head(3)
+
+income_stations['station'] = income_stations.station_name.str.strip().str.upper()
+income_stations.head()
+income_stations.station.unique()
+
+#TODO need to work on this!
+def add_th(s):
+    if any(i in s for i in '0123456789'):
+        # for char in s:
+
+    # if i == '1':
+    #     suffix = 'ST'
+    # elif i == '2':
+    #     suffix = 'ND'
+    # elif i == '3':
+    #     suffix = 'RD'
+    # else:
+    #     suffix = 'TH'
+
+    return any(i in s for i in '0123456789')
+
+
+
+turn_cleaned['station'] = turn_cleaned.station.str.strip()
+
+turn_cleaned.station.unique()
